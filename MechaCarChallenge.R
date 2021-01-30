a9e4dfd676a663348f13722971f75c63d9e863c0
Austin Keeler <akeeler@Austins-MacBook-Air.local>
2021-01-30 17:14:42 -0500

    uploading modified MechaCarChallenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,10 @@
+## Deliverable #1---
 library(dplyr)
 MechaCar <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
 
+## --Deliverable #2---
 MechaCar_Suspension <- read.csv('Suspension_Coil.csv',stringsAsFactors = F)
-total_summary <- group_by(Manufacturing_Lot) %>% summarize(psi, .group = 'keep')
-demo_table2 %>% group_by(condition) %>% summarize(Mean_Mileage=mean(odometer), .groups = 'keep') #create summary table
+total_summary <- MechaCar_Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI) ,Varience=var(PSI), StandardDeviation=sd(PSI), .groups = 'keep')
+lot_summary <- MechaCar_Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI) ,Varience=var(PSI), StandardDeviation=sd(PSI), .groups = 'keep')
