ecbdf1fe51134c4a39eb23a297eb5a734044287d
Austin Keeler <akeeler@Austins-MacBook-Air.local>
2021-01-31 22:22:26 -0500

    Uploading Lot Summaries

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,7 @@ lot_summary <- MechaCar_Suspension %>% group_by(Manufacturing_Lot) %>% summarize
 ##Deliverable #3---
 t.test(log10(MechaCar_Suspension$PSI),mu=1500)
 t.test(log10(lot_summary$Mean),mu=1500)
+# this is T.test on LOT 1
+t.test(subset(MechaCar_Suspension, Manufacturing_Lot == 'Lot1')$PSI, mu=1500)
+t.test(subset(MechaCar_Suspension, Manufacturing_Lot == 'Lot2')$PSI, mu=1500)
+t.test(subset(MechaCar_Suspension, Manufacturing_Lot == 'Lot3')$PSI, mu=1500)
